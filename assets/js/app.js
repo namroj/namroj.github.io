e7d630bc29c980c6dbd01fd23d338ba22d916f31
Jorman Espinoza <jdaniel.espinoza89@gmail.com>
2016-11-13 22:53:47 -0400

    Correccion de enlaces https

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,1 @@
-var app = angular.module('formApp', []);
-
-app.controller('MainCtrl', function ($scope) {
-    $scope.formData = {};
-    
-    $scope.submitForm = function (formData) {
-        alert('Form submitted with' + JSON.stringify(formData));
-    };
-});
+var app = angular.module('contactApp', []);
