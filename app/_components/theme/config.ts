95b3402ee8feef7d52bea50739d81e8d5da2cf28
Jorman Espinoza <espinoza.dev@gmail.com>
2024-03-23 17:40:15 -0300

    style: Adjust theme toggler button style

--- app/_components/theme/config.ts
+++ app/_components/theme/config.ts
@@ -6,7 +6,7 @@ const TOGGLE_SVG_FRAMER_CONFIG = {
 
 const TOGGLE_SVG_PROPS = {
   viewBox: '0 -960 960 960',
-  height: '330',
+  height: '30',
   width: '30',
   xmlns: 'http://www.w3.org/2000/svg',
   className: 'icon'
