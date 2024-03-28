4194f4bd19c228ff452bae1378a880a274d92aa2
Jorman Espinoza <espinoza.dev@gmail.com>
2024-03-28 19:17:29 -0300

    style: Adjust Header and Footer colors

--- app/_components/footer/Footer.tsx
+++ app/_components/footer/Footer.tsx
@@ -1,17 +1,29 @@
+'use client'
+
 import Link from 'next/link'
 import Image from 'next/image'
+
+import {
+  ExpandCollapseState,
+  useExpandCollapseContext
+} from '@/app/_context-providers/expand-collapse/ExpandCollapseProvider'
 
 import nextJsLogo from '@/app/_assets/icons/next.svg'
 import styles from './Footer.module.scss'
 
 export default function Footer() {
+  const { expandCollapseState } = useExpandCollapseContext()
+
+  const getCollapsedOrExpandedState = () =>
+    expandCollapseState === ExpandCollapseState.COLLAPSED ? ExpandCollapseState.EXPANDED : ExpandCollapseState.COLLAPSED
+
   return (
-    <footer className={styles.footer}>
+    <footer className={`${styles.footer} ${styles[getCollapsedOrExpandedState()]}`}>
       <span>
         por <Link href='/'>Jorman</Link> mediante{' '}
       </span>
       <a href='https://nextjs.org/' target='_blank'>
-        <Image src={nextJsLogo} alt='Next.js' width={25} height={25} priority />
+        <Image src={nextJsLogo} alt='Next.js' width={20} height={20} />
       </a>
     </footer>
   )
