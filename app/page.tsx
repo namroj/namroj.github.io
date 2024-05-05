3c66b6feff8626d4d815244dfbf4b0b954dd1b23
Jorman Espinoza <espinoza.dev@gmail.com>
2024-05-05 11:51:16 -0300

    feat(experience): Build data structure for every item

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,19 @@
+import Image from 'next/image'
+
 export default function HomePage() {
-  return <></>
+  return (
+    <Image
+      src='https://pbs.twimg.com/profile_banners/1435415903416500227/1713968806/1500x500'
+      alt='Hero'
+      width={1500}
+      height={500}
+      style={{
+        objectFit: 'cover',
+        objectPosition: 'center',
+        width: '100%',
+        height: 'auto',
+        borderRadius: '1rem',
+      }}
+    />
+  )
 }
