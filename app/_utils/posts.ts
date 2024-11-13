e8e69b537e17b10c361cbf1b3d6513882ff2e6a1
Jorman Espinoza <espinoza.dev@gmail.com>
2024-11-13 08:32:16 -0300

    feat: Add dynamic blog post fetching
    - Added `fs` and `promises` dependencies for file handling.
    - Created `getPosts` utility to fetch blog post metadata dynamically.
    - Updated `BlogPage` to fetch and display post data asynchronously.
    - Included `slug` in MDX files for better routing.

---
+++ app/_utils/posts.ts
@@ -0,0 +1,36 @@
+import fs from 'fs/promises';
+import path from 'path';
+import matter from 'gray-matter';
+
+export type PostMetaData = {
+  title: string;
+  date: string;
+  summary: string;
+  slug: string;
+};
+
+async function getPosts(): Promise<PostMetaData[]> {
+  const postsDirectory = path.join(process.cwd(), 'app/_posts');
+  const filenames = await fs.readdir(postsDirectory);
+
+  const posts = await Promise.all(
+    filenames.map(async (filename) => {
+      const filePath = path.join(postsDirectory, filename);
+      const fileContents = await fs.readFile(filePath, 'utf8');
+      const { data } = matter(fileContents);
+
+      const postMetaData: PostMetaData = {
+        title: data.title as string,
+        date: data.date as string,
+        summary: data.summary as string,
+        slug: data.slug as string,
+      };
+
+      return postMetaData;
+    }),
+  );
+
+  return posts.sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime());
+}
+
+export default getPosts;
--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,16 +1,19 @@
 import { Metadata } from 'next';
 import { Link } from 'next-view-transitions';
 
+import getPosts, { PostMetaData } from '@/utils/posts';
 import { Breadcrumb } from '@/providers/navigation/NavigationProvider';
 import Breadcrumbs from '@/components/main/breadcrumbs/Breadcrumbs';
 import { FaKeyboard } from 'react-icons/fa6';
+import styles from './page.module.scss';
 
 export const metadata: Metadata = {
   title: 'Blog | Jorman Espinoza',
 };
 
-export default function BlogPage() {
+export default async function BlogPage() {
   const breadcrumbs: Breadcrumb[] = [{ label: 'Blog', icon: <FaKeyboard /> }];
+  const posts: PostMetaData[] = await getPosts();
 
   return (
     <>
@@ -18,21 +21,24 @@ export default function BlogPage() {
 
       <h2>Blog</h2>
 
-      <br />
-
-      <Link
-        href="/blog/prueba"
-        style={{ textDecoration: 'underline', fontSize: 'italic' }}
-      >
-        Prueba
-      </Link>
-      <br />
-      <Link
-        href="/blog/domingos"
-        style={{ textDecoration: 'underline', fontSize: 'italic' }}
-      >
-        Domingos
-      </Link>
+      <div className={styles.posts}>
+        {posts.map((post) => (
+          <div key={post.slug}>
+            <div className={styles.post}>
+              <Link
+                href={`/blog/${post.slug}`}
+                style={{ textDecoration: 'underline', fontSize: 'italic' }}
+              >
+                {post.title}
+              </Link>
+              <br />
+              <span style={{ color: 'gray' }}>{post.summary}</span>
+              <br />
+              <span>{post.date}</span>
+            </div>
+          </div>
+        ))}
+      </div>
     </>
   );
 }
