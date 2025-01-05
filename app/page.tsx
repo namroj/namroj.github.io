8efd4978b1a8bf6234fa280f9487ff604840888a
Jorman Espinoza <espinoza.dev@gmail.com>
2025-01-05 16:25:40 -0300

    feat: Add section dividers and enhance styling in page
    - Introduced horizontal rule elements (`<hr>`) after each section in `page.tsx` for improved visual separation.
    - Added a new `.divider` class in `page.module.scss` for consistent styling of the dividers with margins.
    - Adjusted spacing in `.dev` and `.intro` styles, including adding responsive margins for headings.

--- app/page.tsx
+++ app/page.tsx
@@ -90,6 +90,8 @@ export default function HomePage() {
         </ul>
       </div>
 
+      <hr className={styles.divider} />
+
       <section>
         <h4 className={styles.title}>Servicios</h4>
         <ul>
@@ -109,25 +111,35 @@ export default function HomePage() {
         </ul>
       </section>
 
+      <hr className={styles.divider} />
+
       <section className={styles.skills}>
         <h4 className={styles.title}>Arsenal</h4>
         <Skills />
       </section>
+
+      <hr className={styles.divider} />
 
       <section>
         <h4 className={styles.title}>Herramientas</h4>
         <Tools />
       </section>
 
+      <hr className={styles.divider} />
+
       <section>
         <h4 className={styles.title}>Proyectos</h4>
         Algunos <Link href="/projects">proyectos</Link> que he realizado.
       </section>
 
+      <hr className={styles.divider} />
+
       <section>
         <h4 className={styles.title}>Formación</h4>
         Acerca de mi <Link href="/formation">formación</Link>.
       </section>
+
+      <hr className={styles.divider} />
 
       <section>
         <h4 className={styles.title}>Tipografías preferidas</h4>
@@ -156,8 +168,10 @@ export default function HomePage() {
         </ul>
       </section>
 
+      <hr className={styles.divider} />
+
       <p style={{ textAlign: 'right' }}>
-        -- Mi <Link href="/location">camino</Link> de Venezuela a Argentina.
+        -- Mi camino de Venezuela a <Link href="/location">Argentina</Link>.
       </p>
     </>
   );
